0d21232e9fc09c6d1220523449952ca1a003e0f1
Richard Thomson <rthomson@nvidia.com>
2021-03-07 12:48:58 -0700

    Build tutorial "how to scan images"
    - Add target OpenCV::HighGui
    - Add target OpenCV::ImgCodecs

--- cmake/FindOpenCV.cmake
+++ cmake/FindOpenCV.cmake
@@ -8,15 +8,15 @@ if(_OPENCV_CORE_HPP)
     get_filename_component(OpenCV_INCLUDE_DIR ${_OPENCV_CORE_HPP} DIRECTORY)
 endif()
 
-find_library(_OPENCV_CORE_LIB NAMES opencv_core.lib libopencv_core.a
-    PATHS $ENV{OPENCV_DIR}/lib
-    )
-find_library(_OPENCV_CORE_LIB_DEBUG NAMES opencv_cored.lib libopencv_cored.a
-    PATHS $ENV{OPENCV_DIR}/lib
-    )
+find_library(_OPENCV_CORE_LIB NAMES opencv_core.lib libopencv_core.a PATHS $ENV{OPENCV_DIR}/lib)
+find_library(_OPENCV_CORE_LIB_DEBUG NAMES opencv_cored.lib libopencv_cored.a PATHS $ENV{OPENCV_DIR}/lib)
+find_library(_OPENCV_HIGH_GUI_LIB NAMES opencv_highgui.lib libopencv_highgui.a PATHS $ENV{OPENCV_DIR}/lib)
+find_library(_OPENCV_HIGH_GUI_LIB_DEBUG NAMES opencv_highguid.lib libopencv_highguid.a PATHS $ENV{OPENCV_DIR}/lib)
+find_library(_OPENCV_IMG_CODECS_LIB NAMES opencv_imgcodecs.lib libopencv_imgcodecs.a PATHS $ENV{OPENCV_DIR}/lib)
+find_library(_OPENCV_IMG_CODECS_LIB_DEBUG NAMES opencv_imgcodecsd.lib libopencv_imgcodecsd.a PATHS $ENV{OPENCV_DIR}/lib)
 
 # find_package_handle_standard_args(OpenCV REQUIRED_VARS OpenCV_INCLUDE_DIR OpenCV_LIBRARY)
-find_package_handle_standard_args(OpenCV REQUIRED_VARS _OPENCV_CORE_LIB OpenCV_INCLUDE_DIR)
+find_package_handle_standard_args(OpenCV REQUIRED_VARS _OPENCV_CORE_LIB _OPENCV_HIGH_GUI_LIB _OPENCV_IMG_CODECS_LIB OpenCV_INCLUDE_DIR)
 
 if(OpenCV_FOUND)
     add_library(OpenCV::Core STATIC IMPORTED)
@@ -24,5 +24,19 @@ if(OpenCV_FOUND)
     set_property(TARGET OpenCV::Core PROPERTY IMPORTED_LOCATION ${_OPENCV_CORE_LIB})
     if(_OPENCV_CORE_LIB_DEBUG)
         set_property(TARGET OpenCV::Core PROPERTY IMPORTED_LOCATION_DEBUG ${_OPENCV_CORE_LIB_DEBUG})
+    endif()
+
+    add_library(OpenCV::HighGui STATIC IMPORTED)
+    target_include_directories(OpenCV::HighGui INTERFACE ${OpenCV_INCLUDE_DIR})
+    set_property(TARGET OpenCV::HighGui PROPERTY IMPORTED_LOCATION ${_OPENCV_HIGH_GUI_LIB})
+    if(_OPENCV_HIGH_GUI_LIB)
+        set_property(TARGET OpenCV::HighGui PROPERTY IMPORTED_LOCATION_DEBUG ${_OPENCV_HIGH_GUI_LIB_DEBUG})
+    endif()
+
+    add_library(OpenCV::ImgCodecs STATIC IMPORTED)
+    target_include_directories(OpenCV::ImgCodecs INTERFACE ${OpenCV_INCLUDE_DIR})
+    set_property(TARGET OpenCV::ImgCodecs PROPERTY IMPORTED_LOCATION ${_OPENCV_IMG_CODECS_LIB})
+    if(_OPENCV_IMG_CODECS_LIB)
+        set_property(TARGET OpenCV::ImgCodecs PROPERTY IMPORTED_LOCATION_DEBUG ${_OPENCV_IMG_CODECS_LIB_DEBUG})
     endif()
 endif()
